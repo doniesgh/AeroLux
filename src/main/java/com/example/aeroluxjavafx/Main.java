d1db0fbc96ae39bba1ede5b840d2070d282df92b
Doniagh <74374618+doniesgh@users.noreply.github.com>
2024-01-27 21:13:55 +0100

    Premier commit

---
+++ src/main/java/com/example/aeroluxjavafx/Main.java
@@ -0,0 +1,23 @@
+package com.example.aeroluxjavafx;
+
+import com.example.aeroluxjavafx.utils.MyDatabase;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.sql.SQLException;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        MyDatabase db = MyDatabase.getInstance();
+        MyDatabase db2 = MyDatabase.getInstance();
+        System.out.println(db);
+        System.out.println(db2);
+
+    }
+}
---
+++ src/main/java/com/example/aeroluxjavafx/services/IService.java
@@ -0,0 +1,11 @@
+package com.example.aeroluxjavafx.services;
+
+import java.util.List;
+
+public interface IService<T> {
+    void ajouter(T t);
+    void modifier(T t);
+    void supprimer (int id);
+
+    List<T> recuperer();
+}
